5c758499a2af7c1c52ad77a2467dd7aa67f5454b
elias2 <elias2@ymail.com>
2020-08-14 17:58:17 -0400

    2 commit

---
+++ src/test/java/com/spacejet/qa/testcases/addingTest.java
@@ -0,0 +1,14 @@
+package com.spacejet.qa.testcases;
+
+public class addingTest {
+
+	public static void main(String[] args) {
+		int x =20;
+		int y =30;
+		
+		int total =x+y;
+		System.out.println(total);
+
+	}
+
+}
